27ae6f16c3b3ec6b6cd36abfa98315f09a4687f9
stankarp0 <stankarp0@gmail.com>
2019-04-20 23:07:50 +0200

    simple squres printed on svg

--- view.js
+++ view.js
@@ -1,13 +1,13 @@
 class View {
 
-    constructor(facade, grid) {
+    constructor(facade, grid_name) {
         // constans
-        this.img_size = 1000;
+        this.img_size = 900;
         this.click = 0;
 
         // components
         this.facade = facade;
-        this.grid = grid;
+        this.grid_name = grid_name;
 
         // helpers
         this.ns_count = facade.crossings_count_ns;
@@ -15,23 +15,43 @@ class View {
         this.p_count = facade.road_parts_count;
         this.columns_count = (facade.road_parts_count + 1) * this.ew_count;
         this.rows_count = (facade.road_parts_count + 1) * this.ns_count;
-        this.box_width = this.img_size / this.columns_count;
-        this.box_height = this.img_size / this.rows_count;
+        this.box_width = this.img_size / (this.columns_count);
+        this.box_height = this.img_size / (this.rows_count);
 
         // left and right margins - when counts are odd
         this.margin_count = Math.floor(this.p_count / 2);
 
+        d3.select(grid_name).html("");
         this._refresh_data();
-        d3.select(grid).html("");
+        
+        var grid = d3.select(grid_name)
+            .append("svg")
+            .attr("width", this.img_size +"px")
+            .attr("height", this.img_size +"px")
+            .style("display", "block")
+            .style("margin", "auto");
+
+
+        var column = grid.selectAll(".square")
+            .data(this._data)
+            .enter().append("rect")
+            .attr("class","square")
+            .attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; })
+            .attr("width", function(d) { return d.width; })
+            .attr("height", function(d) { return d.height; })
+            .style("fill", function(d) { return d.fill; })
+            .style("stroke", "#000000")
+            .on('click', function(d) {});
     }
     
     _refresh_data() {
-        this.data = [];
+        this._data = [];
         for (let row = 0; row < this.ew_count; row++) {
             for (let column = 0; column < this.ns_count; column++) {
 
                 if (this.facade.has_car_crossing(row, column)) 
-                    this.data.push(this._point_from_crossing(row, column));
+                    this._data.push(this._point_from_crossing(row, column));
 
                 const road_ns = this.facade.get_road_index(row, column, DIRECTION_NS);
                 const road_ew = this.facade.get_road_index(row, column, DIRECTION_EW);
@@ -39,9 +59,9 @@ class View {
                 for(let part = 0; part < this.p_count; part++) {
 
                     if (this.facade.has_car_road_part(road_ns, part)) 
-                        this.data.push(this._point_from_road(row, column, road_ns, part));
+                        this._data.push(this._point_from_road(row, column, road_ns, part));
                     if (this.facade.has_car_road_part(road_ew, part)) 
-                        this.data.push(this._point_from_road(row, column, road_ew, part));
+                        this._data.push(this._point_from_road(row, column, road_ew, part));
                     
                 }
             }
@@ -49,11 +69,13 @@ class View {
     }
 
     _point_from_crossing(row, column) {
-        const x_box = this.margin_count + (1 + this.p_count) * column;
-        const y_box = this.margin_count + (1 + this.p_count) * row;
+        const x_box = (this.margin_count + (1 + this.p_count) * column) % this.columns_count;
+        const y_box = (this.margin_count + (1 + this.p_count) * row) % this.rows_count;
         return {
             x: x_box * this.box_width,
             y: y_box * this.box_height,
+            x_box: x_box,
+            y_box: y_box,
             width: this.box_width,
             height: this.box_height, 
             click: this.click,
@@ -65,8 +87,10 @@ class View {
     _point_from_road(row, column, road, part) {
         const point = this._point_from_crossing(row, column);
         const direction = this.facade.get_road_direction(road);
-        point.x += direction == DIRECTION_NS ? 0 : part * this.box_width;
-        point.y += direction == DIRECTION_EW ? 0 : part * this.box_height;
+        point.x_box = (point.x_box + (direction == DIRECTION_NS ? 0 : part)) % this.columns_count;
+        point.y_box = (point.y_box + (direction == DIRECTION_EW ? 0 : part)) % this.rows_count;
+        point.x = point.x_box * this.box_width;
+        point.y = point.y_box * this.box_height;
         return point;
     }
 
